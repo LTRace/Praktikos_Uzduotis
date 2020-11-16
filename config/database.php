56eab8dfd78637df4aa28494db8ea8e5cdad1610
LTRace <nedas.karaliunas@gmail.com>
2020-11-16 19:08:24 +0200

    hosting

--- config/database.php
+++ config/database.php
@@ -15,7 +15,10 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => ('pgsql'),
+
+    $DATABASE_URL = parse_url('postgres://nlufcncglahvrv:cd59704753cb80d83338c081dc056d35363d26e5baf039387bed9b343e369a57@ec2-46-137-100-204.eu-west-1.compute.amazonaws.com:5432/ddnem87p9k8mqt'),
+
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +68,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://nlufcncglahvrv:cd59704753cb80d83338c081dc056d35363d26e5baf039387bed9b343e369a57@ec2-46-137-100-204.eu-west-1.compute.amazonaws.com:5432/ddnem87p9k8mqt',
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
